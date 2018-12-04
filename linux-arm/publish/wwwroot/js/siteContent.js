81485a8a9965938b7f8798bde6c3b44ff2741c17
Piecyk <adrian.piecyk@dxc.com>
2018-12-04 18:39:13 +0100

    FinalVersionApplication

--- linux-arm/publish/wwwroot/js/siteContent.js
+++ linux-arm/publish/wwwroot/js/siteContent.js
@@ -1,1 +1,37 @@
-﻿
+﻿function myFunction(value) {
+    if (value === 0) {
+        var x = document.getElementById("OneDevice");
+        var y = document.getElementById("TwoDevices");
+        var z = document.getElementById("ThreeDevices");
+
+        x.style.display = "none";
+        y.style.display = "none";
+        z.style.display = "none";
+
+    }
+    if (value === 1) {
+        var x = document.getElementById("OneDevice");
+        var y = document.getElementById("TwoDevices");
+        var z = document.getElementById("ThreeDevices");
+
+        x.style.display = "block";
+        y.style.display = "none";
+        z.style.display = "none";
+    }
+    else if (value === 2) {
+        var x = document.getElementById("OneDevice");
+        var y = document.getElementById("TwoDevices");
+        var z = document.getElementById("ThreeDevices");
+        x.style.display = "block";
+        y.style.display = "block";
+        z.style.display = "none";
+    }
+    else if (value === 3) {
+        var x = document.getElementById("OneDevice");
+        var y = document.getElementById("TwoDevices");
+        var z = document.getElementById("ThreeDevices");
+        x.style.display = "block";
+        y.style.display = "block";
+        z.style.display = "block";
+    }
+}
